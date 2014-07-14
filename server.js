fc6180c32cb71ed2fe5f761fb947bb1a34968003
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2014-07-14 14:55:13 +1200

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,59 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var mongoose = require('mongoose');
 var app     = express();
+
+mongoose.connect('mongodb://localhost/test');
+
+app.get('/', function(req, res){
+	url = 'http://www.victoria.ac.nz/courses/laws/301?year=2014';
+
+	request(url, function(error, response, html){
+		if (!error) {
+			var $ = cheerio.load(html);
+
+			var name ,crn, dit;
+
+			var json = { name: "", crn: "", terms: [] }
+
+			$('.page_title').filter(function(){
+				var data = $(this);
+				name = data.text();
+				json.name = name;
+			})
+
+			$('.course_page').filter(function(){
+				var data = $(this);
+				crn = data.children('h2').first().text();
+				json.crn = crn;
+				var ls = data.children();
+				for(var i = 0; i < ls.length; i++){
+					var d = ls.eq(i).text();
+					if(d.substring(0,8) === "Teaching"){
+						var term = { dates: "", lectures: [] }
+						term.dates = ls.eq(i).text();
+						var lectures = ls.eq(i + 1).children();
+						for (var j = 0; j < lectures.length; j++) {
+							var lecture = {day : "", time: "", room:""};
+							lecture.day = lectures.children().first().children().eq(0).text();
+							lecture.time = lectures.children().first().children().eq(1).text();
+							lecture.room = lectures.children().first().children().eq(2).text();
+							term.lectures.push(lecture);
+						};
+						json.terms.push(term);
+					}
+				}
+			})
+
+			fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+	        	console.log('File successfully written! - Check your project directory for the output.json file');
+	        })
+
+			res.send(json);
+		};
+	})
+})
 
 app.get('/scrape', function(req, res){
 	// Let's scrape Anchorman 2
@@ -36,7 +88,7 @@ app.get('/scrape', function(req, res){
         	console.log('File successfully written! - Check your project directory for the output.json file');
         })
 
-        res.send('Check your console!')
+		res.send(json);
 	})
 })
 
